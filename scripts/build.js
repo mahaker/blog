4b61f53b3aa3aa6535ee7653c61ba831bbc599c6
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2023-04-27 18:22:58 +0900

    layout index.html and posts/*.html

--- scripts/build.js
+++ scripts/build.js
@@ -5,8 +5,10 @@ const stream = require('node:stream')
 const dist = path.resolve(__dirname, '../dist/')
 const entry = path.resolve(__dirname, '../index.html')
 
+const postsMatch = new RegExp('(.html|.md)$')
+
 fs.rmSync(dist, { force: true, recursive: true })
-fs.mkdirSync(dist)
+fs.mkdirSync(path.resolve(dist, 'posts/'), { recursive: true })
 
 // fs.createReadStream(entry, { highWaterMark: 100 })
 fs.createReadStream(entry)
@@ -19,18 +21,32 @@ fs.createReadStream(entry)
   }))
   .pipe(fs.createWriteStream(path.resolve(dist, 'index.html')))
 
+renderPosts()
+
 // NOTE fs.cp is experimental feature
-fs.cpSync(path.resolve(__dirname, '../posts'), path.resolve(dist, 'posts'), { recursive: true }) 
 fs.cpSync(path.resolve(__dirname, '../images'), path.resolve(dist, 'images'), { recursive: true }) 
 
 /**
  * @returns {{ title: string, location: string }[]} - posts
  */
 function getPosts() {
-  const postsMatch = new RegExp('(.html|.md)$')
   const stats = fs.readdirSync(path.resolve(__dirname, '../posts'), { withFileTypes: true })
 
   return stats
     .filter(s => s.isFile() && postsMatch.test(s.name))
     .map(s => ({ title: s.name, location: `/posts/${s.name}` }))
+}
+
+function renderPosts() {
+  const stats = fs.readdirSync(path.resolve(__dirname, '../posts'), { withFileTypes: true })
+  const layout = fs.readFileSync(path.resolve(__dirname, '../layout/base.html')).toString()
+  const contentMatch = new RegExp('<!-- __CONTENT__ -->', 'g')
+
+  stats.forEach(s => {
+    if (s.isDirectory()) return
+    if (!postsMatch.test(s.name)) return
+
+    const rendered = layout.replace(contentMatch, fs.readFileSync(path.resolve(__dirname, '../posts', s.name)))
+    fs.writeFileSync(path.resolve(dist, 'posts/', s.name), rendered)
+  })
 }
