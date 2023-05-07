ec5263e67c614ac552c0c1f145c3977507cf3db0
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2023-05-07 14:59:03 +0900

    favicon support

--- scripts/serve.js
+++ scripts/serve.js
@@ -2,6 +2,11 @@ const fs = require('node:fs')
 const http = require('node:http')
 const path = require('node:path')
 
+const CONTENT_TYPE = {
+  '.html': 'text/html',
+  '.ico': 'image/x-icon',
+}
+
 const server = http.createServer()
 
 server.on('request', (request, res) => {
@@ -10,11 +15,15 @@ server.on('request', (request, res) => {
 
   console.log(next)
 
-  const entry = fs.readFileSync(path.resolve(__dirname, '../dist', next), { encoding: 'utf8' })
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-
-  res.end(entry)
+  try {
+    const entry = fs.readFileSync(path.resolve(__dirname, '../dist', next), { encoding: 'utf8' })
+    res.writeHead(200, { 'Content-Type': CONTENT_TYPE[path.extname(next)] })
+    res.end(entry)
+  } catch(e) {
+    console.error(e)
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end('error occured')
+  }
 })
 server.listen(50000)
 console.log('preview is available on http://localhost:50000')
-
